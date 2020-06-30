53eabc8a8eb87f6d5870f972a4b8edf52bced537
hyeonslove <hyeonslove@kut.ac.kr>
2020-06-30 19:51:22 +0900

    20200630

---
+++ 진법계산기/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace 진법계산기
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "-15";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(188, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "[2]진법";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "변환";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(357, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(170, 21);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "0.41CAC083126E978D4FDF3B64874D8370EB7030A6";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(357, 21);
+            this.textBox4.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(188, 66);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 20);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.Text = "[16]진법";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(294, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "변환";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 118);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 진법계산기/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Text.RegularExpressions;
+
+
+namespace 진법계산기
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        string[] Arr = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        string JinC(string Num1, string Num2, int Jin)
+        {
+            string Re = null;
+            int N2Len = Num2.Length, De = 0;
+            int N1 = 0, N2 = 0;
+            for (int i = 0; i < Num1.Length - N2Len; i++)
+            {
+                Num2 = "0" + Num2;
+            }
+            int x;
+            for (int i = Num1.Length; i > 0; i--)
+            {
+                for (x = 0; x < 26; x++)
+                {
+                    if (Strings.Mid(Num1, i, 1) == Arr[x])
+                    {
+                        N1 = x + 10;
+                        goto N2G;
+                    }
+                } // Num1 A~
+                N1 = Convert.ToInt32(Strings.Mid(Num1, i, 1));
+            N2G:
+                for (x = 0; x < 26; x++)
+                {
+                    if (Strings.Mid(Num2, i, 1) == Arr[x])
+                    {
+                        N2 = x + 10;
+                        goto CLG;
+                    }
+                } // Num2 A~
+                N2 = Convert.ToInt32(Strings.Mid(Num2, i, 1));
+            CLG:
+                if (N1 - N2 - De < 0)
+                {
+                    string Temp = null;
+                    if ((N1 + Jin - De - N2) >= 10)
+                    {
+                        Temp = Arr[(N1 + Jin - De - N2) - 10];
+                    }
+                    else
+                    {
+                        Temp = (N1 + Jin - De - N2).ToString();
+                    }
+                    Re = Temp + Re;
+                    De = 1;
+                }
+                else
+                {
+                    string Temp = null;
+                    if ((N1 - De - N2) >= 10)
+                    {
+                        Temp = Arr[(N1 - De - N2) - 10];
+                    }
+                    else
+                    {
+                        Temp = (N1 - De - N2).ToString();
+                    }
+                    Re = Temp + Re;
+                    De = 0;
+                }
+            }
+            return Re;
+        }
+
+
+        string JinChagne(string Str, int Jin)
+        {
+            string Result = null;
+            int Status = 0;
+            if (Strings.InStr(Str, "-") != 0) // 음수면
+            {
+                Status = 0; // 0음수
+                Str = Strings.Replace(Str, "-", "");
+            }
+            else
+            {
+                Status = 1; // 1양수
+            }
+            Jin = Convert.ToInt32(Strings.Split(Strings.Split(comboBox1.Text, "[")[1], "]")[0]);
+            if (Strings.InStr(Str, ".") != 0)
+            { // 소수점시
+                int Cnt = 0;
+                int Front = Convert.ToInt32(Strings.Split(Str, ".")[0]);
+                if (Front != 0)
+                {
+                    for (int i = 0; Front != 0; i++)
+                    { // 정수처리
+                        if ((Front % Jin) >= 10)
+                            Result = Arr[(Front % Jin) - 10] + Result;
+                        else
+                            Result = (Front % Jin) + Result;
+
+                        Front /= Jin;
+                    }
+                    Result = Result + ".";
+                }
+                else
+                {
+                    Result = Result + "0.";
+                }
+
+
+                double Back = Convert.ToDouble("0." + Strings.Split(Str, ".")[1]);
+                for (int i = 0; Back != 0; i++) // 소수처리
+                {
+                    Back *= Jin;
+                    if ((Convert.ToInt32(Back - 0.5) >= 10))
+                    {
+                        Result = Result + Arr[Convert.ToInt32(Back - 0.499999) - 10];
+                    }
+                    else
+                    {
+                        Result = Result + (Convert.ToInt32(Back - 0.499999));
+                    }
+                    try
+                    {
+                        Back = Back - (Back - Convert.ToDouble("0." + Regex.Split(Back.ToString().Replace(".", "&"), "&")[1]));
+                    }
+                    catch
+                    {
+                        Back -= Convert.ToInt32(Back - 0.499999);
+                    }
+                    // Back = Back - ();
+
+                    if ((++Cnt >= 40) || Back < 0)
+                        break;
+                }
+            }
+            else
+            { // 소수점이 아닐시
+                int Front = Convert.ToInt32(Strings.Split(Str, ".")[0]);
+                for (int i = 0; Front != 0; i++)
+                {
+                    if ((Front % Jin) >= 10)
+                        Result = Result + Arr[(Front % Jin) - 10];
+                    else
+                        Result = (Front % Jin) + Result;
+                    Front /= Jin;
+                }
+            }
+
+            if (Status == 0)
+            { // 음수처리                            
+                Str = textBox1.Text;
+                if (Strings.InStr(Str, ".") != 0) // 소수점시
+                {
+
+                }
+                else
+                { // 소수가 아닐시
+                    int Front = Convert.ToInt32(Strings.Split(Str, ".")[0]);
+                    string Temp = Str;
+                    Temp = Strings.Replace(Temp, "-", "");
+                    int N = JinChagne(Temp,Jin).Length;
+                    Result = "-" + JinC(JinChagne(Math.Pow(Jin, N).ToString(), Jin), Result, Jin);
+                    string Bosu = null;
+                    if (Jin >= 10)
+                    {
+                        Bosu = Arr[Jin - 11];
+                    }
+                    else
+                    {
+                        Bosu = (Jin-1).ToString();
+                    }
+                    Result = "-" + Strings.Replace(Result, "-0", Bosu);
+                }
+            }
+            return Result;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = JinChagne(textBox1.Text, Convert.ToInt32(Strings.Split(Strings.Split(comboBox1.Text,"[")[1],"]")[0]));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 2; i < 37; i++)
+            {
+                comboBox1.Items.Add("[" + i + "]진법");
+                comboBox2.Items.Add("[" + i + "]진법");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Str = textBox3.Text;
+            int Jin = Convert.ToInt32(Strings.Split(Strings.Split(comboBox2.Text, "[")[1], "]")[0]);
+            if (Strings.InStr(Str, ".") != 0)
+            { // 소수점시
+                string Front = Strings.Split(Str, ".")[0]; // 정수처리부분
+                double Sum = 0.0;
+                for (int i = 0; i < Front.Length; i++)
+                {
+                    try
+                    {
+                        Sum += (Math.Pow(Jin, Front.Length - i - 1) * Convert.ToInt32(Strings.Mid(Front, i + 1, 1)));
+                    }
+                    catch
+                    {
+                        int x;
+                        int Temps = 0;
+                        for (x = 0; x < 26; x++)
+                        {
+                            if (Arr[x] == Strings.Mid(Front, i + 1, 1))
+                            {
+                                Temps = x + 10;
+                                break;
+                            }
+                        }
+                        Sum += (Math.Pow(Jin, Front.Length - i - 1) * Temps);
+                    }
+                }
+
+
+                string Back = Strings.Split(Str, ".")[1];
+                textBox4.Text = Sum + "";
+                for (int i = 0; i < Back.Length; i++)
+                {
+                    try
+                    {
+                        Sum += (Math.Pow(Jin, (i + 1) * -1) * Convert.ToInt32(Strings.Mid(Back, i + 1, 1)));
+                    }
+                    catch
+                    {
+                        int x;
+                        int Temps = 0;
+                        for (x = 0; x < 26; x++)
+                        {
+                            if (Arr[x] == Strings.Mid(Back, i + 1, 1))
+                            {
+                                Temps = x + 10;
+                                break;
+                            }
+                        }
+                        Sum += (Math.Pow(Jin, (i + 1) * -1) * Temps);
+                    }
+                }
+                textBox4.Text = Sum + "";
+            }
+            else
+            { // 소수점 아니면
+                double Sum = 0.0;
+                for (int i = 0; i < Str.Length; i++)
+                {
+                    try
+                    {
+                        Sum += (Math.Pow(Jin, Str.Length - i - 1) * Convert.ToInt32(Strings.Mid(Str, i + 1, 1)));
+                    }
+                    catch
+                    {
+                        int x;
+                        for (x = 0; x < 36; x++)
+                        {
+                            if (Strings.Mid(Str, i + 1, 1) == Arr[x])
+                            {
+                                break;
+                            }
+                        }
+
+                        Sum += (Math.Pow(Jin, Str.Length - i - 1) * (10 + x));
+                    }
+                }
+                textBox4.Text = Sum + "";
+            }
+
+        }
+    }
+}
